11f6f765dc42f55e6802b7b6151df6a77c5e5791
Peterson Wagner <peterson.w.k.carvalho@gmail.com>
2017-09-17 14:23:03 -0300

    Update pgmnega.c

--- pgmnega.c
+++ pgmnega.c
@@ -3,6 +3,16 @@
 #include "pgmlib.h"
 #include "commline.h"
 
+//funcao que aplica o filtro negativo
+//substituindo cada valor da matriz por
+//Valor Maximo - Valor do Pixel
+void apply_negative (PGM_t *pgm)
+{
+	for (int i = 0; i < (*pgm).rows; ++i)
+		for (int j = 0; j < (*pgm).cols; ++j)
+			(*pgm).matrix[i][j] = (*pgm).maxval - (*pgm).matrix[i][j];
+}
+
 int main(int argc, char *argv[])
 {
 	PGM_t pgm;
